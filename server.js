867ceeb53c7eac14393b32a6fcab2d5356528c06
Dorkiee <ma19kdo@gmail.com>
2023-03-08 17:33:19 +0000

    workpls test

--- server.js
+++ server.js
@@ -43,6 +43,13 @@ app.use((err, req, res, next) => {
   })
 })
 
+app.use(express.static(path.join(__dirname, '/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/build/index.html'));
+});
+
+
 
 app.listen(process.env.PORT || port, () => console.log("server is up and running"))
 
