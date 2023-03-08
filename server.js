91b8b7c0a46187c6e8d5a7701ff2a273d2744a05
Dorkiee <ma19kdo@gmail.com>
2023-03-08 17:42:05 +0000

    attempt 1

--- server.js
+++ server.js
@@ -43,10 +43,9 @@ app.use((err, req, res, next) => {
   })
 })
 
-app.use(express.static(path.join(__dirname, '/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/build/index.html'));
+  res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 
